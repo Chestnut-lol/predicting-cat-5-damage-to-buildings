8e40340af7e3028d72d3298ce48f285b30f6e9cc
Chestnut-lol <chestnut2tasty@gmail.com>
2022-09-25 14:38:27 +0100

    Modify utils.py

--- src/data_loading/utils.py
+++ src/data_loading/utils.py
@@ -6,6 +6,7 @@ PATH_TO_DATA = os.path.join(PATH_TO_DIR, "data")
 PATH_TO_DATA_RAW = os.path.join(PATH_TO_DATA, "raw")
 PATH_TO_DATA_PROCESSED = os.path.join(PATH_TO_DATA, "processed")
 PATH_TO_PATCHES = os.path.join(PATH_TO_DATA_PROCESSED, "patches")
+PATH_TO_GEOJSONS = os.path.join(PATH_TO_DATA_PROCESSED, "processed")
 
 FILE_LIST_PREFIX = "https://raw.githubusercontent.com/Chestnut-lol/predicting-cat-5-damage-to-buildings/main/data/raw/digital-globe-file-lists/" 
 FILE_LIST_SUFFIX = "_file_list.txt" 
